8c88f244b2fda82188feb5faff6c09430dc4347b
T-Tron <palamantv@gmail.com>
2022-04-19 10:24:36 +0200

    V2 Prepare
    Its The New Disign For The New WebSite With A Beautiful Code ;)

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Phoenix Network - Accueil</title>
+        <meta name="description" content="Le site internet officiel de queenwoow !">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="icon" type="image/png" href="assets/img/logo.png">
+        <link rel="stylesheet" href="assets/css/index.css">
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:500&display=swap" rel="stylesheet">
+    </head>
+    <body>
+        <header>
+            <a class="logo" href="/"><img src="assets/img/logo.png" alt="logo"></a>
+            <nav>
+                <ul class="nav__links">
+                    <li><a href="index.php">Home</a></li>
+                    <li><a href="#">Blog</a></li>
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">ChangeList</a></li>
+                    <li><a href="#">Classement</a></li>
+                    <li><a href="#">Support</a></li>
+                </ul>
+            </nav>
+            <a class="cta" href="#">Login</a>
+            <p class="menu cta">Menu</p>
+        </header>
+        <div class="overlay">
+            <a class="close">&times;</a>
+            <div class="overlay__content">
+                <a href="index.php">Home</a>
+                <a href="">Blog</a>
+                <a href="#">Gallery</a>
+                <a href="#">ChangeList</a>
+                <a href="#">Classement</a>
+                <a href="#">Support</a>
+                <a href="#">Login</a>
+            </div>
+        </div>
+        <script type="text/javascript" src="assets/js/mobile.js"></script>
+    </body>
+</html>
